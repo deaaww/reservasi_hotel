4063f66d36eb47a98262151d7a442dc849e7cdda
deaaww <deamarsell12@gmail.com>
2025-12-01 10:11:59 +0700

    database

---
+++ config/Database.php
@@ -0,0 +1,27 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_PORT', '5433');
+define('DB_NAME', 'reservasi_hotel');
+define('DB_USER', 'postgres');
+define('DB_PASS', '121205');
+
+class Database {
+    private $conn;
+    
+    public function connect() {
+        $this->conn = null;
+        
+        try {
+            $dsn = "pgsql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME;
+            $this->conn = new PDO($dsn, DB_USER, DB_PASS);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch(PDOException $e) {
+            echo "Connection Error: " . $e->getMessage();
+            die();
+        }
+        
+        return $this->conn;
+    }
+}
+?>

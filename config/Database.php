a2d5028407975cdb1f64b6e581431c8bce8fcf09
deaaww <deamarsell12@gmail.com>
2025-12-04 13:09:57 +0700

    dashboard

--- config/Database.php
+++ config/Database.php
@@ -1,9 +1,9 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_PORT', '5432');
+define('DB_PORT', '5433');
 define('DB_NAME', 'reservasi_hotel');
 define('DB_USER', 'postgres');
-define('DB_PASS', '123');
+define('DB_PASS', '121205');
 
 class Database {
     private $conn;

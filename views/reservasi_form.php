9dabc4eb5e45d0d99489cd02266232b4ae7bc6cf
deaaww <deamarsell12@gmail.com>
2025-12-05 18:42:31 +0700

    hapus checklist

--- views/reservasi_form.php
+++ views/reservasi_form.php
@@ -220,30 +220,6 @@ try {
                 </div>
             </div>
         </div>
-                
-        <div class="card table-card mt-3">
-            <div class="card-body">
-                <h6 class="mb-3"><i class="bi bi-list-check me-2"></i>Checklist:</h6>
-                <div class="form-check mb-2">
-                    <input class="form-check-input" type="checkbox" id="check1">
-                    <label class="form-check-label" for="check1">
-                        Data tamu sudah benar
-                    </label>
-                </div>
-                <div class="form-check mb-2">
-                    <input class="form-check-input" type="checkbox" id="check2">
-                    <label class="form-check-label" for="check2">
-                        Tanggal sudah sesuai
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="check3">
-                    <label class="form-check-label" for="check3">
-                        Total pembayaran sudah dicek
-                    </label>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 
